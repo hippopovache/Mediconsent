cc7a6cee98d3a8e70c3cf03bb65d5bb4a1e4c0aa
maxime belle <45199116+hippopovache@users.noreply.github.com>
2021-01-26 09:07:14 +0100

    a

--- app/src/main/java/com/ort/mediconsent/domain/repository/UserRepository.kt
+++ app/src/main/java/com/ort/mediconsent/domain/repository/UserRepository.kt
@@ -1,6 +1,6 @@
 package com.ort.mediconsent.domain.repository
 
 interface UserRepository {
-    //suspend sous entend que le coe utilise co routines (async)
+    //suspend sous entend que le code utilise co routines (async)
     suspend fun getUserConnect(id: Int, mdp: String): Boolean
 }
--- app/src/main/java/com/ort/mediconsent/presentation/MainActivity.kt
+++ app/src/main/java/com/ort/mediconsent/presentation/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.ort.mediconsent.presentation
 
 import android.os.Bundle
+import android.text.TextUtils.replace
 import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.commit
 import com.ort.mediconsent.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.review)
+        setContentView(R.layout.connect)
+    }
+
+    fun displaySearchLayout(){
+        supportFragmentManager.commit{
+            replace(R.id.fragment_container, )
+        }
     }
 }
